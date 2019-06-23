23b8638b45ec7943cfc8c27f58dde98b6a36a02b
unknown <asher>
2019-06-23 20:42:40 +0500

    Module 2 assignment

---
+++ module2-solution/app.js
@@ -0,0 +1,50 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+  toBuy.buy = function(index) {
+    ShoppingListCheckOffService.buyItem(index);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  var toBuyItems = [
+    { name: "cookies", quantity: 13 },
+    { name: "Juice", quantity:20 },
+    { name: "Fruit", quantity: 50 },
+    { name: "Medicine", quantity: 25 },
+    { name: "Bun", quantity: 21 }];
+
+  var boughtItems = [];
+
+  service.buyItem = function(index) {
+    boughtItems.push(toBuyItems[index]);
+    toBuyItems.splice(index, 1);
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+})();
