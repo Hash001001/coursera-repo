53a92f918fef30292338fe02d3092e615815ad29
unknown <asher>
2019-06-23 19:07:21 +0500

    web file

--- app3.js
+++ app3.js
@@ -3,18 +3,36 @@
   'use strict';
 
   angular.module('MsgApp',[])
-  .controller('MsgController', MyMsgController);
-MyMsgController.$inject=['$scope'];
-  function MyMsgController($scope) {
+  .controller('MsgController', MyMsgController)
+  .filter('change',filterFactory);
+MyMsgController.$inject=['$scope','changeFilter'];
+  function MyMsgController($scope,changeFilter) {
   $scope.name="Ashir";
   $scope.changeValue="regular";
 
   $scope.changePic=function () {
     $scope.changeValue="formal";
-
-
   };
 
+$scope.mynameFun= function () {
+  return "Hello beta kase ho ??"
+};
+
+$scope.filterFunction= function () {
+  var msg="Hello beta kase ho ??"
+  msg=changeFilter(msg);
+  return msg;
+};
+
+  }
+
+  function filterFactory() {
+    return function (input) {
+      input= input || "";
+      input=input.replace('beta','Ashir');
+      return input;
+
+    };
   }
 
 })();
